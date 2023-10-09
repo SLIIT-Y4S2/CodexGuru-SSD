2e5ac854b634adfcd0e01309f97b3f64bf3ec8ca
vibhashan <ranhindavibhashana@gmail.com>
2023-10-10 02:32:33 +0530

    report download feature done, report halfway done

---
+++ codexguru-client/src/app/exam-reports/page.tsx
@@ -0,0 +1,73 @@
+/**
+ * This is the exam reports page
+ */
+
+"use client";
+
+import React, { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Layout, Button, theme } from "antd";
+import Link from "next/link";
+import InstructorSideBar from "@/components/exams-instructor/InstructorSideBar";
+import BreadCrumbs from "@/components/common/BreadCrumbs";
+import Reports from "@/components/exams-instructor/Reports";
+
+const { Header, Sider, Content } = Layout;
+
+export default function ExamsPageLayout() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+
+        <InstructorSideBar />
+      </Sider>
+
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: "15px",
+            }}
+          >
+            <BreadCrumbs
+              linkList={[
+                {
+                  title: <Link href="#">Dashboard</Link>,
+                },
+                {
+                  title: "Reports",
+                },
+              ]}
+            />
+          </div>
+        </Header>
+
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Reports />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+}
